60268d442b621f1e18858e056f53d2abddcf7d3c
Henrique Douglas <henriquedouglas2803@gmail.com>
2024-10-03 22:03:33 -0300

    adiciona camera

--- src/pages/MemoriasDoPassado.tsx
+++ src/pages/MemoriasDoPassado.tsx
@@ -1,11 +1,77 @@
 import { StatusBar } from 'expo-status-bar';
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Image, SafeAreaView, StyleSheet, Text, TextInput, TouchableHighlight } from 'react-native';
+
+import * as ImagePicker from 'expo-image-picker';
 
 export default function MemoriasDoPassado() {
+
+  const [name, setName] = useState('')
+  const [image, setImage] = useState('')
+
+
+  async function getImageLibrary() {
+
+    const permission = await ImagePicker.requestMediaLibraryPermissionsAsync()
+    
+    if (permission.granted) {
+      const imageInLibrary = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.All,
+        orderedSelection: true
+      })
+
+      if (imageInLibrary?.assets) {
+        setImage(imageInLibrary.assets[0].uri)
+      }
+
+    }
+  }
+
+  async function getImageCamera() {
+
+    const permission = await ImagePicker.requestCameraPermissionsAsync()
+
+    if (permission.granted) {
+      const imageInCamera = await ImagePicker.launchCameraAsync()
+
+      if (imageInCamera?.assets) {
+        setImage(imageInCamera.assets[0].uri)
+      }
+    }
+
+  }
+
   return (
     <SafeAreaView style={styles.container}>
-      <Text>PASSADO</Text>
       <StatusBar style="auto" />
+
+      {
+        image && <Image style={styles.image} source={{ uri: image }} width={150} height={150} />
+      }
+
+      <TextInput
+        style={styles.input}
+        value={name}
+        onChangeText={setName}
+      />
+
+      <TouchableHighlight
+        underlayColor="#e596dd"
+        style={styles.imageButton}
+        onPress={getImageLibrary}
+      >
+        <Text>Escolher imagem</Text>
+      </TouchableHighlight>
+
+
+      <TouchableHighlight
+        underlayColor="#e596dd"
+        style={styles.imageButton}
+        onPress={getImageCamera}
+      >
+        <Text>Abrir camera</Text>
+      </TouchableHighlight>
+
     </SafeAreaView>
   );
 }
@@ -13,6 +79,27 @@ export default function MemoriasDoPassado() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff'
+    backgroundColor: '#fff',
+    padding: 20
   },
+  input: {
+    borderWidth: 1,
+    width: '100%',
+    borderColor: '#f216dc',
+    height: 32,
+    padding: 5
+  },
+  imageButton: {
+    backgroundColor: '#f216dc',
+    height: 32,
+    width: '100%',
+    borderRadius: 16,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginVertical: 10
+  },
+  image: {
+    alignSelf: 'center',
+    marginVertical: 10
+  }
 });
--- src/pages/MemoriasDoPresente.tsx
+++ src/pages/MemoriasDoPresente.tsx
@@ -1,11 +1,73 @@
 import { StatusBar } from 'expo-status-bar';
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { Button, Image, SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { CameraView, CameraType } from 'expo-camera'
+import { MaterialCommunityIcons } from '@expo/vector-icons'
+import { useState, useRef } from 'react';
 
 export default function MemoriasDoPresente() {
+
+  const [typeCamera, setTypeCamera] = useState<CameraType>('front')
+  const [myPhoto, setMyPhoto] = useState('')
+
+  const cameraRef = useRef<CameraView>()
+
+  function flipCamera() {
+    if (typeCamera === "front") {
+      setTypeCamera('back')
+    } else {
+      setTypeCamera('front')
+    }
+  }
+
+  async function captureImage() {
+    const myImage = await cameraRef.current?.takePictureAsync({
+      quality: 1,
+    })
+
+    if (myImage?.uri) {
+      setMyPhoto(myImage?.uri)
+    }
+  }
+
+
   return (
     <SafeAreaView style={styles.container}>
-      <Text>memorias do presente</Text>
       <StatusBar style="auto" />
+
+      {
+        myPhoto === '' ?
+          <CameraView
+            style={styles.camera}
+            facing={typeCamera}
+            ref={cameraRef}
+          >
+            <View style={styles.footerCameraContainer}>
+
+              <TouchableOpacity style={styles.buttonCapture} onPress={flipCamera}>
+                <MaterialCommunityIcons
+                  name='camera-flip'
+                  color="red"
+                  size={30}
+                />
+              </TouchableOpacity>
+
+              <TouchableOpacity style={styles.buttonCapture} onPress={captureImage}>
+                <MaterialCommunityIcons
+                  name='camera'
+                  color="red"
+                  size={30}
+                />
+              </TouchableOpacity>
+            </View>
+
+          </CameraView>
+          : <View>
+            <Image source={{ uri: myPhoto }} style={{width: '100%', height: '80%'}} />
+            <Button title='Resetar' onPress={() => setMyPhoto('')} />
+          </View>
+      }
+
+
     </SafeAreaView>
   );
 }
@@ -15,4 +77,25 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff'
   },
+  camera: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'flex-end'
+  },
+  buttonCapture: {
+    width: 50,
+    height: 50,
+    borderRadius: 50,
+    backgroundColor: '#FFF',
+    marginBottom: 10,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  footerCameraContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    width: '100%',
+    padding: 20
+  }
 });
